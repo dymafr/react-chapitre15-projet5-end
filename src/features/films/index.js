82f4e22465efce63ff5efa7589be8e2be4c0202b
Erwan Vallée <erwanp.vallee@gmail.com>
2021-01-15 17:22:10 +0100

    update

--- src/features/films/index.js
+++ src/features/films/index.js
@@ -2,26 +2,25 @@ import React from 'react';
 import { MovieList, MovieDetails, SearchBar } from './components';
 import Loading from '../../components/utils/Loading';
 
-export default (props) => {
+const Films = (props) => {
   return (
     <>
-      <SearchBar updateMovies={ props.updateMovies } />
-      { props.loaded ? (
-        <div className="d-flex flex-row flex-fill pt-4 p-2" >
-          <MovieList 
-            movies={ props.movies } 
-            updateSelectedMovie={ props.updateSelectedMovie }
-            favoris={ props.favoris.map( f => f.title ) }
-            removeFavori={ props.removeFavori }
-            addFavori={ props.addFavori }
+      <SearchBar updateMovies={props.updateMovies} />
+      {props.loaded ? (
+        <div className="d-flex flex-row flex-fill pt-4 p-2">
+          <MovieList
+            movies={props.movies}
+            updateSelectedMovie={props.updateSelectedMovie}
+            favoris={props.favoris.map((f) => f.title)}
+            removeFavori={props.removeFavori}
+            addFavori={props.addFavori}
           />
-          <MovieDetails 
-            movie={ props.movies[props.selectedMovie] }
-          />
+          <MovieDetails movie={props.movies[props.selectedMovie]} />
         </div>
       ) : (
         <Loading />
-      ) }
+      )}
     </>
-  )
-}
+  );
+};
+export default Films;
