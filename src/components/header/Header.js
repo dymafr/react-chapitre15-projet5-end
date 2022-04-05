fe8ada2bd4a579f134c5140f0c119213b4e43baa
Quentin <quentindevial19@gmail.com>
2022-04-05 18:53:08 +0200

    header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,26 +1,51 @@
-import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 export default class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+  toggleHeader = () => {
+    this.setState({ open: !this.state.open });
+  };
 
   render() {
     return (
       <header className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">AlloMovie</a>
-        <button className="navbar-toggler">
+        <a className="navbar-brand" href="/">
+          AlloMovie
+        </a>
+        <button onClick={this.toggleHeader} className="navbar-toggler">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse">
+        <div className={`navbar-collapse ${this.state.open ? "" : "collapse"}`}>
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <NavLink to="/films" className="nav-link" activeClassName="active" > Home </NavLink>
+              <NavLink
+                to="/films"
+                className="nav-link"
+                activeClassName="active"
+              >
+                {" "}
+                Home{" "}
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to="/favoris" className="nav-link" activeClassName="active"> Favoris </NavLink>
+              <NavLink
+                to="/favoris"
+                className="nav-link"
+                activeClassName="active"
+              >
+                {" "}
+                Favoris{" "}
+              </NavLink>
             </li>
           </ul>
         </div>
       </header>
     );
   }
-
 }
